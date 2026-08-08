54148d20030e43dcdeeffc4ea41b17ba54065005
Dirgaaa <dirgayuditama6@gmail.com>
2026-08-09 03:53:10 +0700

    feat(landing): implement hero section

--- src/feature/(user)/home/containers/HomeContainer.tsx
+++ src/feature/(user)/home/containers/HomeContainer.tsx
@@ -1,21 +1,16 @@
 import Image from "next/image";
-import Hero from "../components/Hero";
-import Kegiatan from "../components/Kegiatan/Kegiatan";
 import CaturPattern from "@/assets/user/pembatas.svg";
+import FaqSection from "../components/FAQ/FaqSection";
+import Hero from "../components/Hero/HeroSection";
+import Kegiatan from "../components/Kegiatan/Kegiatan";
 import ProdiSection from "../components/Prodi/ProdiSection";
 import RangkaianSection from "../components/Rangkaian/RangkaianSection";
-import FaqSection from "../components/FAQ/FaqSection";
+import "../styles/animation.css";
 
 const HomeContainer = () => {
   return (
     <main className="overflow-hidden">
       <Hero />
-      <Image
-        src={CaturPattern}
-        alt="Pattern"
-        className="w-full bg-secondary-500 h-18 object-cover md:h-auto"
-        aria-hidden="true"
-      />
       <Kegiatan />
       <ProdiSection />
       <Image
