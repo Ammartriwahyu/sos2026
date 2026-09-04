f741a628a148eccde2a055fb1e82516c2607605b
Ammar Triwahyu <ammartriwahyu@gmail.com>
2026-09-05 00:44:14 +0700

    chore: delete unsed font

--- src/shared/utils/font.ts
+++ src/shared/utils/font.ts
@@ -8,39 +8,10 @@ export const upanddownNormal = localFont({
     },
   ],
   variable: "--font-upanddownnormal",
+  display: "swap",
 });
 export const poppins = localFont({
   src: [
-    {
-      path: "../../assets/fonts/poppins/Poppins-Thin.woff",
-      weight: "100",
-      style: "normal",
-    },
-    {
-      path: "../../assets/fonts/poppins/Poppins-ThinItalic.woff",
-      weight: "100",
-      style: "italic",
-    },
-    {
-      path: "../../assets/fonts/poppins/Poppins-ExtraLight.woff",
-      weight: "200",
-      style: "normal",
-    },
-    {
-      path: "../../assets/fonts/poppins/Poppins-ExtraLightItalic.woff",
-      weight: "200",
-      style: "italic",
-    },
-    {
-      path: "../../assets/fonts/poppins/Poppins-Light.woff",
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "../../assets/fonts/poppins/Poppins-LightItalic.woff",
-      weight: "300",
-      style: "italic",
-    },
     {
       path: "../../assets/fonts/poppins/Poppins-Regular.woff",
       weight: "400",
@@ -57,19 +28,9 @@ export const poppins = localFont({
       style: "normal",
     },
     {
-      path: "../../assets/fonts/poppins/Poppins-MediumItalic.woff",
-      weight: "500",
-      style: "italic",
-    },
-    {
       path: "../../assets/fonts/poppins/Poppins-SemiBold.woff",
       weight: "600",
       style: "normal",
-    },
-    {
-      path: "../../assets/fonts/poppins/Poppins-SemiBoldItalic.woff",
-      weight: "600",
-      style: "italic",
     },
     {
       path: "../../assets/fonts/poppins/Poppins-Bold.woff",
@@ -77,30 +38,11 @@ export const poppins = localFont({
       style: "normal",
     },
     {
-      path: "../../assets/fonts/poppins/Poppins-BoldItalic.woff",
-      weight: "700",
-      style: "italic",
-    },
-    {
       path: "../../assets/fonts/poppins/Poppins-ExtraBold.woff",
       weight: "800",
       style: "normal",
     },
-    {
-      path: "../../assets/fonts/poppins/Poppins-ExtraBoldItalic.woff",
-      weight: "800",
-      style: "italic",
-    },
-    {
-      path: "../../assets/fonts/poppins/Poppins-Black.woff",
-      weight: "900",
-      style: "normal",
-    },
-    {
-      path: "../../assets/fonts/poppins/Poppins-BlackItalic.woff",
-      weight: "900",
-      style: "italic",
-    },
   ],
   variable: "--font-poppins",
+  display: "swap",
 });
